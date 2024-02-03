5a5cc73ea8574683b0d79de1cd721c868bf0b2fb
boldPro <probold777@gmail.com>
2024-02-03 11:22:22 -0500

    passed variables to index.heex

--- pass_gen/lib/pass_gen_web/controllers/page_controller.ex
+++ pass_gen/lib/pass_gen_web/controllers/page_controller.ex
@@ -2,6 +2,15 @@ defmodule PassGenWeb.PageController do
   use PassGenWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    # render(conn, "index.html")
+    password_lenghts = [
+      Weak: Enum.map(1..15, & &1),
+      Strong: Enum.map(16..88, & &1),
+      Unreal: [100, 150]
+    ]
+
+    password = ""
+
+    render(conn, "index.html", password_length: password_lenghts, password: password )
   end
 end
